a6a4894fb48be0a1e73dae1829b2fe27bc0c8ec5
Serg <sergejancev26@gmail.comgit config --listgit config --global user.email sergejancev26@gmail.com>
2023-11-23 18:32:57 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -1,12 +1,54 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-  console.log("Геолокація не підтримується вашим браузером");
+function getTasks() {
+  return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+
+function saveTasks(tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function displayTasks() {
+  const tasks = getTasks();
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = tasks.map(task => `<li>${task} <button onclick="deleteTask('${task}')">Видалити</button></li>`).join('');
+}
+
+function addNewTask() {
+  const newTask = document.getElementById('newTaskInput').value.trim();
+  if (newTask) {
+    const tasks = getTasks();
+    tasks.push(newTask);
+    saveTasks(tasks);
+    displayTasks();
+    document.getElementById('newTaskInput').value = '';
+  } else {
+    alert('Введіть текст для нової таски!');
+  }
+}
+
+function deleteTask(task) {
+  const tasks = getTasks().filter(item => item !== task);
+  saveTasks(tasks);
+  displayTasks();
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    alert('Геолокація не підтримується вашим браузером.');
+  }
 }
 
 function showPosition(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  
-  console.log(`Ваше місцезнаходження: широта ${latitude} і довгота ${longitude}`);
+  alert(`Ваша локація: ${latitude}, ${longitude}`);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  displayTasks();
+
+  document.getElementById('addTaskBtn').addEventListener('click', addNewTask);
+  
+  getLocation();
+});
